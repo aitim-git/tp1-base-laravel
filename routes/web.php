b8ec89912cdb45e1fccb5a7ce58e55773e162a62
NiaikoVersioning <niaikoversioning@gmail.com>
2025-07-15 09:37:51 +0300

    Ajout page about

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome', ['version' => '1.0']);
 });
+
+Route::get('/about', function () {
+    return 'À propos';
+});
