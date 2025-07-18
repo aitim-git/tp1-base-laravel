ad45c65e19a2435d2ab12e080542ae79b8815dce
aitim-git <mitiairene64@gmail.com>
2025-07-18 14:54:28 +0300

    2eme commit dans feat/auth

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,4 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return 'À propos';
 });
+Route::get('/login', [AuthController::class, 'showLoginForm']);
